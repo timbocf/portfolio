f2e702761a012d2f4ce08b54697fef8649d954e3
Tim Rodgers <timbocf@gmail.com>
2020-09-18 20:33:23 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
-  <title>Tim Rodgers Portfolio</title>
+  <title>Tim Rodgers-Web Developer</title>
 </head>
 <body>
 <div class="container">
   <div class="row">
-    <div class="col-sm navbar-brand" id="sitehead"></div>
-    <div class="col-sm sitenav"></div>
+    <div class="col-sm navbar" id="sitehead">
+      <div class="col-sm navbar-brand"></div>
+      <div class="col-sm sitenav"></div>
+    </div>
   </div>
 </div>
 
@@ -18,6 +21,11 @@
   <div class="jumbotron"></div>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 <script src="js/script.js"></script>
+
 </body>
 </html>
