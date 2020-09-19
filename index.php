146b456ea5aebbeb12e4ad35582b1c3b3e5d6760
Tim Rodgers <timbocf@gmail.com>
2020-09-18 20:54:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/style.css">
   <title>Tim Rodgers Portfolio</title>
 </head>
 <body>
-  <h1>Tim Rodgers Portfolio</h1>
-  <script src="js/script.js"></script>
+<div class="container">
+  <div class="row">
+    <div class="col-sm navbar-brand" id="sitehead"></div>
+    <div class="col-sm sitenav"></div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="jumbotron"></div>
+</div>
+
+<script src="js/script.js"></script>
 </body>
 </html>
