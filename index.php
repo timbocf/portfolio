3451ebe2b63dd5e437e86870d9424eaccd0a09bb
Tim Rodgers <timbocf@gmail.com>
2020-09-19 20:48:49 -0700

    Updated nav menu spacing and made contact button

--- index.php
+++ index.php
@@ -5,23 +5,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="icon" href="images/plug-sideways.svg" type="image/svg" sizes="16x16">
   <title>Tim Rodgers-Web Developer</title>
+  <script src="https://kit.fontawesome.com/cf1fb4309f.js" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="container">
-  <div class="row">
-    <div class="col-sm navbar" id="sitehead">
-      <div class="col-sm navbar-brand">
-        <a href="#"><img src="icon.png" title="Tim Rodgers logo"></a>
+
+<div class="navbar" id="sitehead" role="navigation" aria-label="main navigation">
+  <div class="container">
+    <div class="navbar-brand">
+      <a href="#" alt="Tim Rodgers | Front-End Developer with Divine Connections Media"><i class="fas fa-plug" id="logo"></i></a>
+    </div>
+    <a class="navbar-burger" role="button" aria-label="menu" aria-expanded="false" data-target="#navMenu"><span aria-hidden="true"></span><span aria-hidden="true"></span><span aria-hidden="true"></span></a>
+    
+    <div class="navbar-menu" id="navMenu">
+      <div class="navbar-start"></div>
+      <div class="navbar-end">
+        <a class="navbar-item" href="#">About</a>
+        <a class="navbar-item" href="#">Work</a>
+        <div class="navbar-item">
+          <a href="#" class="contact-button">Contact</a>
+        </div>
       </div>
-      <div class="col-sm sitenav"></div>
     </div>
   </div>
 </div>
 
-<div class="jumbotron jumbotron-fluid">
-  <div class="container"></div>
-</div>
+
+<div class="container"></div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
 
