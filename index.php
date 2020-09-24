f7ae36c198b299dcfd37e7ead364d9cea8a8a0ab
RODGERS <TRODGERS@azcorrections.local>
2020-09-24 16:05:49 -0700

    updated with new icon and links

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
-  <link rel="icon" href="https://raw.githubusercontent.com/timbocf/portfolio/no-jumbotron/plug-solid.svg" type="image/svg" sizes="16x16">
+  <link rel="icon" href="https://raw.githubusercontent.com/timbocf/portfolio/master/images/divine-connections-icon.svg" type="image/svg" sizes="16x16">
   <title>Tim Rodgers-Web Developer</title>
   <script src="https://kit.fontawesome.com/cf1fb4309f.js" crossorigin="anonymous"></script>
 </head>
@@ -14,7 +14,7 @@
 <div class="navbar" id="sitehead" role="navigation" aria-label="main navigation">
   <div class="container">
     <div class="navbar-brand col-sm">
-      <a href="/portfolio" alt="Tim Rodgers | Front-End Developer with Divine Connections Media"><i class="fas fa-plug" id="logo"></i></a>
+      <a href="#" alt="Tim Rodgers | Front-End Developer with Divine Connections Media"><i class="fas fa-plug" id="logo"></i></a>
     </div>
     <a class="navbar-burger" role="button" aria-label="menu" aria-expanded="false" data-target="#navMenu"><span aria-hidden="true"></span><span aria-hidden="true"></span><span aria-hidden="true"></span></a>
     
@@ -27,10 +27,10 @@
         -->
       </div>
       <div class="navbar-end">
-        <a class="navbar-item" href="#">About</a>
-        <a class="navbar-item" href="#">Work</a>
+        <a class="navbar-item" href="#about">About</a>
+        <a class="navbar-item" href="#work">Work</a>
         <div class="navbar-item">
-          <a href="#" class="contact-button" id="contact-button">Contact</a>
+          <a href="#contact" class="contact-button" id="contact-button">Contact</a>
         </div>
       </div>
     </div>
