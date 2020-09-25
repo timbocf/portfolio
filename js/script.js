ca9d1045b955e10a521e81c4edae04fe545e87eb
timbocf <timbocf@gmail.com>
2020-09-24 19:14:18 -0700

    completed slider and added relevant images

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+var i = 0;
+var images = ['Tim-Rodgers-Web-Developer.jpg', '4him.png', 'classic-stitch.png', 'CSI.png', 'fitness-divas.png', 'gym-101.png', 'new-life-home.png', 'revee-lamar.png', 'riobata-home.png', 'Set-Free.png', 'setfree-landing-page.png', 'Set-Free-layout.jpg'];
+
 $(function(){
-	$('.navbar-brand').hover(function(){
-		$('#brand').toggle();
+	$('#slider').css('background-image', 'images/Tim-Rodgers-Web-Developer.jpg');
+})
+
+$(function(){
+	$('#leftbtn').click(function(){
+		i--;
+		i = i < 0 ? images.length - 1 : i;
+		$('#slider').css('background-image', 'url("images/' + images[i] + '")');
+	})
+})
+
+$(function(){
+	$('#rightbtn').click(function(){
+		i++;
+		i = i > images.length - 1 ? 0 : i;
+		$('#slider').css('background-image', 'url("images/' + images[i] + '")');
 	})
 })
