c6ec6beaee238e74c14c162c0ee7000f97a4fce9
timbocf <timbocf@gmail.com>
2020-09-25 20:33:51 -0700

    got the menu working again by copying all the head scripts and styles from the w3schools menu and now it works but the styles are screwed up. will have to fix it

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 	$('body').scrollspy({target: ".navbar", offset: 50});   
   
 	// Add smooth scrolling on all links inside the navbar
-	$("#myNavbar a").on('click', function(event) {
+	$("#navMenu a").on('click', function(event) {
 	  // Make sure this.hash has a value before overriding default behavior
 	  if (this.hash !== "") {
 		// Prevent default anchor click behavior
